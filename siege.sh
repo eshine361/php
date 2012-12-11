f5ab559070568897b57336344abd0488224b92d5
yizhou <yi.zhou@i-md.com>
2012-12-11 18:23:56 +0800

    add

---
+++ siege.sh
@@ -0,0 +1,13 @@
+siege \
+--header='GET /login/123/yizhou/13800000000 HTTP/1.1
+Host: 192.168.1.224:19017
+Connection: keep-alive
+Cache-Control: max-age=0
+User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
+Accept-Encoding: gzip,deflate,sdch
+Accept-Language: en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4
+Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.3
+Cookie: u="Su1Zw2Q0PR7BIi6FFcMg5Rzh977y2L1zkw0NZAelRqY="; eventId=GETest; token=4sC+Y6hEf05dVm2Xc0GmuJ6V92lpzQ6R4o3lC1ew2SovrQVA+j7DadWlefk5y9o6' \
+--concurrent=1 \
+--file="test.url"
